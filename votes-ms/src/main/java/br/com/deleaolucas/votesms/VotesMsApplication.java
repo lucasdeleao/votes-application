9388d4f92480b28ec6412f9d5b9410602397bc27
ilegra <lucasdeleao>
2020-08-10 18:31:22 -0300

    refactoring code

--- votes-ms/src/main/java/br/com/deleaolucas/votesms/VotesMsApplication.java
+++ votes-ms/src/main/java/br/com/deleaolucas/votesms/VotesMsApplication.java
@@ -1,9 +1,13 @@
 package br.com.deleaolucas.votesms;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
+@EnableAutoConfiguration
 public class VotesMsApplication {
 
 	public static void main(String[] args) {
